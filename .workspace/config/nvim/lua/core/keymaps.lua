64227b56379715cdc2a4dce05b65f2ee5ec96f6a
chenlianghong <clh021@gmail.com>
2023-09-11 23:23:02 +0800

    .workspace plugins-setup ok

--- .workspace/config/nvim/lua/core/keymaps.lua
+++ .workspace/config/nvim/lua/core/keymaps.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 keymap.set("i", "jk", "<ESC>")
+keymap.set("n", ";", ":")
 keymap.set("n", "<leader>sv", "<C-w>v")
 keymap.set("n", "<leader>sh", "<C-w>s")
 
