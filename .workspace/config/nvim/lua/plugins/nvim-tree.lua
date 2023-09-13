c4f81237a59e687af2b8ac50530086bc0911aa9f
chenlianghong <clh021@gmail.com>
2023-09-13 10:57:43 +0800

    nvim-tree: keymap self config file

--- .workspace/config/nvim/lua/plugins/nvim-tree.lua
+++ .workspace/config/nvim/lua/plugins/nvim-tree.lua
@@ -1,4 +1,9 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+local keymap = vim.keymap
+keymap.set("n", "<leader>ft", ":NvimTreeToggle<CR>")
+keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>")
+keymap.set("n", "<leader>e", ":NvimTreeFocus<CR>")
+
 require("nvim-tree").setup()
