a43a240b9ab7b7219c2b341531d5b56bf043fc46
chenlianghong <clh021@gmail.com>
2023-09-14 15:43:05 +0800

    plug: update which-key

--- .workspace/config/nvim/lua/lianghong/plugins/which-key.lua
+++ .workspace/config/nvim/lua/lianghong/plugins/which-key.lua
@@ -10,4 +10,32 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
   },
+  config = function()
+    require("which-key").setup({})
+    local wk = require("which-key")
+    wk.register({
+      l = {"<cmd>Lazy<cr>", "Lazy"},
+      y = {'\'"+y', "Copy to Clipboard"},
+      -- s = {'w', "Save"},
+    }, { prefix = "<leader>" })
+    wk.register({
+      e = { name = "+Explorer" },
+      f = { name = "+Finder" },
+      h = { name = "+Harpoon" },
+      n = { name = "+Not height" },
+      o = { name = "+Swap" },
+      t = { name = "+Tab" },
+      w = { name = "+Win&Session" },
+      g = {
+        name = "+LSP",
+        g = {"<cmd>Lspsaga lsp_finder<cr>", "lsp finder"},
+        r = {"<cmd>Lspsaga rename<cr>", "Rename"},
+        a = {"<cmd>Lspsaga code_action<cr>", "Code action"},
+        p = {"<cmd>Lspsaga peek_definition<cr>", "Peek definition"},
+        d = {"<cmd>Lspsaga goto_definition<cr>", "Goto definition"},
+        o = {"<cmd>Lspsaga outline<cr>", "File Outline"},
+        n = {"<cmd>lua vim.diagnostic.goto_next()<cr>", "Goto next diagnostic"}
+      }
+    }, {prefix="<leader>"})
+  end,
 }
