c0e2ef2ac9722b9948fb5ba88ddfc902a3f655a2
chenlianghong <clh021@gmail.com>
2023-09-14 11:31:02 +0800

    lsp: update

--- .workspace/config/nvim/lua/plugins/lsp.lua
+++ .workspace/config/nvim/lua/plugins/lsp.lua
@@ -1,19 +1,31 @@
 require("mason").setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 
-require("mason-lspconfig").setup {
-    ensure_installed = { "lua_ls", "bashls" },
-}
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"lua_ls",
+		"bashls",
+		"tsserver",
+		"cssls",
+		"dockerls",
+		"emmet_ls",
+		"html",
+		"jsonls",
+		"pyright",
+		"yamlls",
+		"gopls",
+	},
+})
 
 -- 使 cmp 与 lsp 结合
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-require("lspconfig").lua_ls.setup {
-  capabilities = capabilities,
-}
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+require("lspconfig").lua_ls.setup({
+	capabilities = capabilities,
+})
