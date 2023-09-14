52859c9b423fed4dca57dd5d74febd637b5f630d
chenlianghong <clh021@gmail.com>
2023-09-14 16:52:18 +0800

    keymap: leader tabnext Num

--- .workspace/config/nvim/lua/lianghong/core/keymaps.lua
+++ .workspace/config/nvim/lua/lianghong/core/keymaps.lua
@@ -20,15 +20,15 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
--- keymap.set("n", "<leader>1", ":tabnext 1<CR>")
--- keymap.set("n", "<leader>2", ":tabnext 2<CR>")
--- keymap.set("n", "<leader>3", ":tabnext 3<CR>")
--- keymap.set("n", "<leader>4", ":tabnext 4<CR>")
--- keymap.set("n", "<leader>5", ":tabnext 5<CR>")
--- keymap.set("n", "<leader>6", ":tabnext 6<CR>")
--- keymap.set("n", "<leader>7", ":tabnext 7<CR>")
--- keymap.set("n", "<leader>8", ":tabnext 8<CR>")
--- keymap.set("n", "<leader>9", ":tabnext 9<CR>")
+keymap.set("n", "<leader>1", ":tabnext 1<CR>")
+keymap.set("n", "<leader>2", ":tabnext 2<CR>")
+keymap.set("n", "<leader>3", ":tabnext 3<CR>")
+keymap.set("n", "<leader>4", ":tabnext 4<CR>")
+keymap.set("n", "<leader>5", ":tabnext 5<CR>")
+keymap.set("n", "<leader>6", ":tabnext 6<CR>")
+keymap.set("n", "<leader>7", ":tabnext 7<CR>")
+keymap.set("n", "<leader>8", ":tabnext 8<CR>")
+keymap.set("n", "<leader>9", ":tabnext 9<CR>")
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 -- keymap.set("n", "<leader>ft", "<C-w>v:Ntree<CR>")
