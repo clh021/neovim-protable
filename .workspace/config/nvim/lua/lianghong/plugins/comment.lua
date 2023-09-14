2240dff622ed1a08920486456f9570c9377aad11
chenlianghong <clh021@gmail.com>
2023-09-14 16:50:12 +0800

    plug: fix comment key

--- .workspace/config/nvim/lua/lianghong/plugins/comment.lua
+++ .workspace/config/nvim/lua/lianghong/plugins/comment.lua
@@ -1,19 +1,23 @@
 return {
   "numToStr/Comment.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "JoosepAlviste/nvim-ts-context-commentstring",
+  opts = {
+      -- add any options here
   },
-  config = function()
-    -- import comment plugin safely
-    local comment = require("Comment")
-
-    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
-
-    -- enable comment
-    comment.setup({
-      -- for commenting tsx and jsx files
-      pre_hook = ts_context_commentstring.create_pre_hook(),
-    })
-  end,
+  lazy = false,
+--  event = { "BufReadPre", "BufNewFile" },
+--  dependencies = {
+--    "JoosepAlviste/nvim-ts-context-commentstring",
+--  },
+--  config = function()
+--    -- import comment plugin safely
+----    local comment = require("Comment")
+----
+----    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+----
+----    -- enable comment
+----    comment.setup({
+----      -- for commenting tsx and jsx files
+----      pre_hook = ts_context_commentstring.create_pre_hook(),
+----    })
+--  end,
 }
